47eb79648204535267cb2524457c43d15b399599
Riza <orduhaniriza@gmail.com>
2025-05-28 10:07:39 +0300

    prefinal 2

---
+++ frontend/src/shims-vue-router.d.ts
@@ -0,0 +1,10 @@
+// src/shims-vue-router.d.ts
+import { ComponentCustomProperties } from 'vue'
+import { Router, RouteLocationNormalizedLoaded } from 'vue-router'
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $router: Router
+    $route: RouteLocationNormalizedLoaded
+  }
+}
